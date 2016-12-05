7955bce5cfb432887a19c45eb4c7dce1c72d1c88
Gustavo Isaias Chalco Apaza <gchalcoa@gmail.com>
2016-12-04 19:49:52 -0500

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Gustavo Isaias Chalco Apaza on 12/4/16.
+//  Copyright © 2016 Gustavo Isaias Chalco Apaza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
