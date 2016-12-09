3287b62421a420ac858eefc224048b77a564bb8a
Gustavo Isaias Chalco Apaza <gchalcoa@gmail.com>
2016-12-08 23:47:37 -0500

    Cambios en projecto

---
+++ BullCowGame/FBullCowGame.cpp
@@ -0,0 +1,9 @@
+//
+//  FBullCowGame.cpp
+//  BullCowGame
+//
+//  Created by Gustavo Isaias Chalco Apaza on 12/8/16.
+//  Copyright © 2016 Gustavo Isaias Chalco Apaza. All rights reserved.
+//
+
+#include "FBullCowGame.hpp"
---
+++ BullCowGame/FBullCowGame.hpp
@@ -0,0 +1,14 @@
+//
+//  FBullCowGame.hpp
+//  BullCowGame
+//
+//  Created by Gustavo Isaias Chalco Apaza on 12/8/16.
+//  Copyright © 2016 Gustavo Isaias Chalco Apaza. All rights reserved.
+//
+
+#ifndef FBullCowGame_hpp
+#define FBullCowGame_hpp
+
+#include <stdio.h>
+
+#endif /* FBullCowGame_hpp */
