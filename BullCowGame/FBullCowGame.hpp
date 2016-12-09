824813c33713bdbf43856e9c8677ba128163bb04
Gustavo Isaias Chalco Apaza <gchalcoa@gmail.com>
2016-12-08 23:48:36 -0500

    Prototipos de la Clase BullCowGame

--- BullCowGame/FBullCowGame.hpp
+++ BullCowGame/FBullCowGame.hpp
@@ -9,6 +9,39 @@
 #ifndef FBullCowGame_hpp
 #define FBullCowGame_hpp
 
-#include <stdio.h>
+#include <string>
+
+using FString = std::string;
+using int32 = int;
+
+struct FBullCowCount {
+    int32 Bulls = 0;
+    int32 Cows = 0;
+};
+
+class FBullCowGame {
+    // Metodos publicos
+public:
+    FBullCowGame(); // Constructor
+    void Reset(); // TODO Mejorar el valor de retorno
+    int32 GetMaxTries() const;
+    int32 GetCurrentTry() const;
+    int32 GetHiddenWordLength() const;
+    bool IsGameWon() const;
+    bool CheckGuessValidity(FString) const;
+    
+    FBullCowCount SubmitGuess(FString);
+    
+    // Metodos Privados
+private:
+    // Iniciados en constructor
+    int32 MyCurrentTry;
+    int32 MyMaxTries;
+    FString MyHiddenWord;
+    
+    //bool IsIsogram();
+
+};
+
 
 #endif /* FBullCowGame_hpp */
